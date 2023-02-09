6b0f1bd9c932993c1d510374f69a9026554f016c
LEL <lel@mail.org>
2023-02-09 17:18:33 +0100

    Fix contract name

--- Voting.sol
+++ Voting.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.18;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract Admin is Ownable{
+contract Voting is Ownable{
 
     //States variables definitions
     Vote[] voteHistory;
